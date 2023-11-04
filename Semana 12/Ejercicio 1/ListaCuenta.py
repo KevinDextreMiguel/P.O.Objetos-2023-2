2b1cbf7092f9f2f4490c8f11f7aeed749ca8e9f2
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2023-11-03 22:29:47 -0500

    Update ListaCuenta.py

--- Semana 12/Ejercicio 1/ListaCuenta.py
+++ Semana 12/Ejercicio 1/ListaCuenta.py
@@ -1,65 +1,56 @@
 from CuentaCorriente import*
 
-
-class ListaCuentas:
+class ListaCuenta():
     def __init__(self):
-        self.__listaCuentas=[]
+        self.__lista = []
+    
+    def insertarCuenta(self,objCuenta):
+        self.__lista.append(objCuenta)
         
-    def imprimir(self):
-      for i in range(len(self.__listaCuentas)):
-        self.__listaCuentas[i].consultar_cuenta()
-    
-    def InsertarCuenta(self,objetoCuenta):
-       self.__listaCuentas.append(objetoCuenta)
-    
-    def realizar_transferencia(self,dni_emisor,dni_receptor,monto):
-      pos1 = pos2 = -1  
-      for i in range(len(self.__listaCuentas)):
-        if dni_emisor == self.__listaCuentas[i].get_dni():
-          pos1 = i
-        if dni_receptor == self.__listaCuentas[i].get_dni():
-          pos2 = i
-      
-      
-      self.__listaCuentas[pos1].retirar_dinero(monto)
-      if self.__listaCuentas[pos1].get_saldo()>=monto:
-        self.__listaCuentas[pos2].ingresar_dinero(monto)
-        print('Operacion exitosa')
-      print('Operación denegada')
-    
-    def saldo_promedio_distrito(self,distrito):
-      promedio = cont = 0
-      for i in range(len(self.__listaCuentas)):
-        if distrito == self.__listaCuentas[i].get_distrito():
-          promedio +=self.__listaCuentas[i].get_saldo()
-          cont += 1
-    
-      if cont != 0:
-        promedio = promedio / cont
-    
-      print('El saldo promedio del distrito de ',distrito,'es: ',promedio)
-    
-    def apellido_letra(self,apellidos):
-      for i in range(len(self.__listaCuentas)):
-        aux = self.__listaCuentas[i].get_apellidos()
-        if aux[0] == apellidos[0]:
-          print('\n',self.__listaCuentas[i].consultar_cuenta())
-    
-    def mayor_saldo(self):
-      mayor = -1
-      for i in range(len(self.__listaCuentas)):
-        if self.__listaCuentas[i].get_saldo() > mayor:
-          mayor = self.__listaCuentas[i].get_saldo()
-    
-      print('El o las personas que tienen mayor saldo son:')
-      for i in range(len(self.__listaCuentas)):
-        if mayor == self.__listaCuentas[i].get_saldo():
-          print(self.__listaCuentas[i].consultar_cuenta())
-    
-
-
-
-
-
-
-
+    def MostrarTodos(self):
+        for i in range(len(self.__lista)):
+            self.__lista[i].consultarCuenta()
+            
+    def realizarTRansferencia(self,dniEmisor,dniReceptor,monto):
+        pos1=pos2=-1
+        for i in range(len(self.__lista)):
+            if self.__lista[i].getDni() == dniEmisor:
+                pos1=i              
+            if self.__lista[i].getDni() == dniReceptor:
+                pos2=i
+                
+        if self.__lista[pos1].getSaldo()>=monto:
+            self.__lista[pos1].retirarMonto(monto)
+            self.__lista[pos2].ingresarMonto(monto)
+            print('Con exito')
+        else:
+            print('Operacion denegada')
+            
+    def SaldoPromedioDistrito(self,distrito):
+        promedio=cont=0
+        for i in range(len(self.__lista)):
+            if self.__lista[i].getdistrito()==distrito:
+                promedio+=self.__lista[i].getSaldo()
+                cont+=1
+        
+        if cont != 0:
+            promedio = promedio/cont
+        print(f'El saldo promedio del distrito {distrito} es de: {promedio}')
+                
+        
+    def DatosAppellidoInicial(self,apellido):
+        for i in range(len(self.__lista)):
+           aux = self.__lista[i].getapellidos()
+           if aux[0]==apellido[0]:
+               self.__lista[i].consultarCuenta()
+                
+    def PersonasSaldoMayor(self):
+       mayor = -1
+       for i in range(len(self.__lista)):
+           if self.__lista[i].getSaldo() > mayor:
+               mayor = self.__lista[i].getSaldo()
+               
+       print('Las personas con saldo mayor')
+       for i in range(len(self.__lista)):
+           if self.__lista[i].getSaldo() == mayor:
+               self.__lista[i].consultarCuenta()
