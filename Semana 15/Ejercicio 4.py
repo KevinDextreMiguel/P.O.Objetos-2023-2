a0836533a97ae31e9aeba3b7eaab193905bba135
KevinDextreMiguel <123123234+KevinDextreMiguel@users.noreply.github.com>
2023-11-24 22:28:28 -0500

    Update Ejercicio 4.py

--- Semana 15/Ejercicio 4.py
+++ Semana 15/Ejercicio 4.py
@@ -1,6 +1,13 @@
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def generar(n,lista):
+    for i in range(n):
+        v=random.randint(2000,4000)
+        lista.append(v)
+        
+    
 def grafico_lineas_ingresos_gastos(datos):
     # Definimos la figura y los ejes del gráfico con Matplotlib
     fig, ax = plt.subplots()
@@ -9,12 +16,16 @@ def grafico_lineas_ingresos_gastos(datos):
     # Añadimos la escala del eje y
     ax.set_ylim([0, max(datos.Ingresos.max(), datos.Gastos.max()) + 500])
     # Añadimos el título
-    plt.title('Evolución de ingresos y gastos')
+    plt.title('Evolucion de ingresos y gastos')
     # Devolvemos el objeto con los ejes y el gráfico que contienten
     #return ax
 
-datos = {'Mes':['Ene', 'Feb', 'Mar', 'Abr'], 'Ingresos':[4500, 5200, 4800, 5300],
-         'Gastos':[2300, 2450, 2000, 2200]}
+n=4
+ingresos = []
+gastos = []
+generar(n,ingresos)
+generar(n,gastos)
+datos = {'Mes':['Ene', 'Feb', 'Mar', 'Abr'], 'Ingresos':ingresos,'Gastos':gastos}
 df_datos = pd.DataFrame(datos).set_index('Mes')
 #print(df_datos)
 grafico_lineas_ingresos_gastos(df_datos)
